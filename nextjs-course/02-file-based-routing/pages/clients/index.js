aff994c40c36c9533470dba406de3a8d34012c40
dedybastiant <dedybastian.t@gmail.com>
2022-12-08 14:44:10 +0700

    Change Link with pathname and query on client page

--- nextjs-course/02-file-based-routing/pages/clients/index.js
+++ nextjs-course/02-file-based-routing/pages/clients/index.js
@@ -10,9 +10,12 @@ function ClientsPage() {
         <div>
             <h1>The Clients Page</h1>
             <ul>
-                {clients.map(client => 
+                {clients.map(client =>
                     <li key={client.id}>
-                        <Link href={`/clients/${client.id}`}>{client.name}</Link>
+                        <Link href={{
+                            pathname: "/clients/[id]",
+                            query: { id: client.id }
+                        }}>{client.name}</Link>
                     </li>
                 )}
             </ul>
