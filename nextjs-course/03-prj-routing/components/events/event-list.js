6a339510cb176cd9acd498028bbc5bfa0291a798
dedybastiant <dedybastian.t@gmail.com>
2022-12-13 20:30:07 +0700

    Add event-item and event-list component with module css

---
+++ nextjs-course/03-prj-routing/components/events/event-list.js
@@ -0,0 +1,17 @@
+import EventItem from "./event-item";
+import classes from './event-list.module.css'
+
+function EventList(props) {
+    const { items } = props
+    return (
+        <div className={classes.list}>
+            <ul>
+                {items.map(event =>
+                    <EventItem key={event.id} title={event.title} date={event.date} image={event.image} location={event.location} id={event.id} />
+                )}
+            </ul>
+        </div>
+    )
+}
+
+export default EventList;
