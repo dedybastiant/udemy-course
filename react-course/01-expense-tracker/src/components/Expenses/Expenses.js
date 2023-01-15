d98411933f237556e701a4e021cad01ec4abd119
dedybastiant <dedybastian.t@gmail.com>
2023-01-15 18:09:55 +0700

    Add ExpenseFilter for filtering the year

--- react-course/01-expense-tracker/src/components/Expenses/Expenses.js
+++ react-course/01-expense-tracker/src/components/Expenses/Expenses.js
@@ -1,12 +1,20 @@
+import { useState } from "react";
 import Card from "../UI/Card";
 import ExpenseItem from "./ExpenseItem";
 import "./Expenses.css";
+import ExpensesFilter from "./ExpensesFilter";
 
 function Expenses(props) {
+  const [filteredYear, setFilteredYear] = useState("2020");
   const { expenses } = props;
+
+  const filterChangeHandler = (selectedYear) => {
+    setFilteredYear(selectedYear);
+  };
 
   return (
     <Card className="expenses">
+      <ExpensesFilter selected={filteredYear} onChangeFilter={filterChangeHandler} />
       {expenses.map((expense) => (
         <ExpenseItem
           key={expense.id}
