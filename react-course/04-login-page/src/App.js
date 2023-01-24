bf6b6fc490afd16d71067cd83c316674034e4408
dedybastiant <dedybastian.t@gmail.com>
2023-01-24 17:02:47 +0700

    Using context to share state and function

--- react-course/04-login-page/src/App.js
+++ react-course/04-login-page/src/App.js
@@ -1,46 +1,23 @@
-import React, { useEffect, useState } from "react";
+import React, { useContext } from "react";
 
 import Login from "./components/Login/Login";
 import Home from "./components/Home/Home";
 import MainHeader from "./components/MainHeader/MainHeader";
-import AuthContext from "./store/auth-context";
+import {AuthContext} from "./store/auth-context";
 
 function App() {
-  const [isLoggedIn, setIsLoggedIn] = useState(false);
-
-  useEffect(() => {
-    const iSLoggedInInformation = localStorage.getItem("isLoggedIn");
-    if (iSLoggedInInformation === "1") {
-      setIsLoggedIn(true);
-    }
-  }, []);
-
-  const loginHandler = (email, password) => {
-    // We should of course check email and password
-    // But it's just a dummy/ demo anyways
-    localStorage.setItem("isLoggedIn", "1");
-    setIsLoggedIn(true);
-  };
-
-  const logoutHandler = () => {
-    localStorage.removeItem("isLoggedIn");
-    setIsLoggedIn(false);
-  };
+  const authCtx = useContext(AuthContext);
 
   return (
     <React.Fragment>
-      <AuthContext.Provider
-        value={{
-          isLoggedIn: isLoggedIn,
-          logoutHandler: logoutHandler,
-        }}
-      >
-        <MainHeader isAuthenticated={isLoggedIn} onLogout={logoutHandler} />
-        <main>
-          {!isLoggedIn && <Login onLogin={loginHandler} />}
-          {isLoggedIn && <Home onLogout={logoutHandler} />}
-        </main>
-      </AuthContext.Provider>
+      <MainHeader
+        isAuthenticated={authCtx.isLoggedIn}
+        onLogout={authCtx.logoutHandler}
+      />
+      <main>
+        {!authCtx.isLoggedIn && <Login />}
+        {authCtx.isLoggedIn && <Home />}
+      </main>
     </React.Fragment>
   );
 }
--- react-course/04-login-page/src/store/auth-context.js
+++ react-course/04-login-page/src/store/auth-context.js
@@ -1,8 +1,42 @@
-import { createContext } from "react";
+import { createContext, useEffect, useState } from "react";
 
 const AuthContext = createContext({
   isLoggedIn: false,
   logoutHandler: () => {},
+  loginHandler: (email, password) => {},
 });
 
-export default AuthContext;
+const AuthContextProvider = (props) => {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+  useEffect(() => {
+    const iSLoggedInInformation = localStorage.getItem("isLoggedIn");
+    if (iSLoggedInInformation === "1") {
+      setIsLoggedIn(true);
+    }
+  }, []);
+
+  const loginHandler = (email, password) => {
+    localStorage.setItem("isLoggedIn", "1");
+    setIsLoggedIn(true);
+  };
+
+  const logoutHandler = () => {
+    localStorage.removeItem("isLoggedIn");
+    setIsLoggedIn(false);
+  };
+
+  return (
+    <AuthContext.Provider
+      value={{
+        isLoggedIn: isLoggedIn,
+        loginHandler: loginHandler,
+        logoutHandler: logoutHandler,
+      }}
+    >
+      {props.children}
+    </AuthContext.Provider>
+  );
+};
+
+export { AuthContext, AuthContextProvider };
