df6489b68df628ab24b5389724ecc209fa9a934d
dedybastiant <dedybastian.t@gmail.com>
2023-02-01 22:32:53 +0700

    Change to firebase for saving the data

--- react-course/06-movies-list/src/App.js
+++ react-course/06-movies-list/src/App.js
@@ -19,15 +19,17 @@ function App() {
         throw new Error("Something went wrong");
       }
       const data = await response.json();
-      const transformedMovies = await data.results.map((movie) => {
-        return {
-          id: movie.episode_id,
-          title: movie.title,
-          openingText: movie.opening_crawl,
-          releaseDate: movie.release_date,
-        };
-      });
-      setMovies(transformedMovies);
+      const loadedMovie = [];
+
+      for (const key in data) {
+        loadedMovie.push({
+          id: key,
+          title: data[key].title,
+          openingText: data[key].openingText,
+          releaseDate: data[key].releaseDate,
+        });
+      }
+      setMovies(loadedMovie);
     } catch (err) {
       setError(err.message);
     }
@@ -38,9 +40,19 @@ function App() {
     fetchMoviesHandler();
   }, [fetchMoviesHandler]);
 
-  function addMovieHandler(movie) {
-    console.log(movie);
-  }
+  const addMovieHandler = async (movie) => {
+    const response = await fetch(
+      "https://react-http-d457d-default-rtdb.asia-southeast1.firebasedatabase.app/movies.json",
+      {
+        method: "POST",
+        body: JSON.stringify(movie),
+        headers: {
+          "Content-Type": "application/json",
+        },
+      }
+    );
+    // console.log(response);
+  };
 
   let content = <h2>Found no movies.</h2>;
 
