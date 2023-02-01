d36a4e6157c5f8d57882abdf40c4c8d483b8ab6a
dedybastiant <dedybastian.t@gmail.com>
2023-02-01 22:14:09 +0700

    Add component for adding movie

--- react-course/06-movies-list/src/App.js
+++ react-course/06-movies-list/src/App.js
@@ -2,6 +2,7 @@ import React, { useCallback, useEffect, useState } from "react";
 
 import MoviesList from "./components/MoviesList";
 import "./App.css";
+import AddMovie from "./components/AddMovie";
 
 function App() {
   const [movies, setMovies] = useState([]);
@@ -11,7 +12,9 @@ function App() {
   const fetchMoviesHandler = useCallback(async () => {
     setIsLoading(true);
     try {
-      const response = await fetch("https://swapi.dev/api/films");
+      const response = await fetch(
+        "https://react-http-d457d-default-rtdb.asia-southeast1.firebasedatabase.app/movies.json"
+      );
       if (!response.ok) {
         throw new Error("Something went wrong");
       }
@@ -35,6 +38,10 @@ function App() {
     fetchMoviesHandler();
   }, [fetchMoviesHandler]);
 
+  function addMovieHandler(movie) {
+    console.log(movie);
+  }
+
   let content = <h2>Found no movies.</h2>;
 
   if (movies.length > 0) {
@@ -51,6 +58,9 @@ function App() {
 
   return (
     <React.Fragment>
+      <section>
+        <AddMovie onAddMovie={addMovieHandler} />
+      </section>
       <section>
         <button onClick={fetchMoviesHandler}>Fetch Movies</button>
       </section>
