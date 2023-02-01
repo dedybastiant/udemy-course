b8a73544dcbd701a237c279bf392f92b7c9e603c
dedybastiant <dedybastian.t@gmail.com>
2023-02-01 21:56:24 +0700

    Add useEffect for fetchin data when user open the page

--- react-course/06-movies-list/src/App.js
+++ react-course/06-movies-list/src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useCallback, useEffect, useState } from "react";
 
 import MoviesList from "./components/MoviesList";
 import "./App.css";
@@ -8,7 +8,7 @@ function App() {
   const [isLoading, setIsLoading] = useState(false);
   const [error, setError] = useState(null);
 
-  const fetchMoviesHandler = async () => {
+  const fetchMoviesHandler = useCallback(async () => {
     setIsLoading(true);
     try {
       const response = await fetch("https://swapi.dev/api/films");
@@ -29,7 +29,11 @@ function App() {
       setError(err.message);
     }
     setIsLoading(false);
-  };
+  }, []);
+
+  useEffect(() => {
+    fetchMoviesHandler();
+  }, [fetchMoviesHandler]);
 
   let content = <h2>Found no movies.</h2>;
 
