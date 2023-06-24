7e29f9a18182828296166b5fec85897e7961af8a
dedybastiant <dedybastian.t@gmail.com>
2023-06-24 14:09:05 +0700

    add outlets and action on controller

---
+++ swift-course/Tipsy-iOS13/Tipsy/Controllers/ResultViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ResultViewController.swift
+//  Tipsy
+//
+//  Created by Dedy Dita on 24/06/23.
+//  Copyright © 2023 The App Brewery. All rights reserved.
+//
+
+import UIKit
+
+class ResultViewController: UIViewController {
+    
+    @IBOutlet weak var totalLabel: UILabel!
+    @IBOutlet weak var settingsLabel: UILabel!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+    @IBAction func recalculatePressed(_ sender: UIButton) {
+        
+    }
+
+}
